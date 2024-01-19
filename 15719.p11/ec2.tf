435fe1511d29a89e754e6d5f6127c4b43f94808b
Lingfei He <harryinhurryhelium@gmail.com>
2024-01-19 01:08:17 -0500

    add 1 instance

--- 15719.p11/ec2.tf
+++ 15719.p11/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "cmucc" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  count                  = 1
+  count                  = 2
   # key_name               = "terraform-cloud-key"
   # user_data              = file("install.sh")
   vpc_security_group_ids = [aws_security_group.student_ami_sg.id,]
