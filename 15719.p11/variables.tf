daee7ab3a7a456107ac8721090006c5cb6517013
Lingfei He <harryinhurryhelium@gmail.com>
2024-01-19 01:03:45 -0500

    mod var

--- 15719.p11/variables.tf
+++ 15719.p11/variables.tf
@@ -7,3 +7,13 @@ variable "instance_type" {
   description = "EC2 Instance Type "
   type        = string
 }
+
+variable "ami_id" {
+  description = "EC2 Instance ami_id "
+  type        = string
+}
+
+variable "project_tag" {
+  description = "project_tag"
+  type        = string
+}
