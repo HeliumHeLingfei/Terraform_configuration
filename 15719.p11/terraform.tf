cf55404c90a70b1eb4d49ca0dc60d1cb66d4b0e5
unknown <harryinhurryhelium@gmail.com>
2024-01-19 00:18:41 -0500

    init

---
+++ 15719.p11/terraform.tf
@@ -0,0 +1,20 @@
+terraform { 
+	cloud {
+    organization = "18709"
+
+
+    workspaces {
+      name = "15719_p11"
+      project  = "18709"
+    }
+
+
+  }
+  required_providers {
+    aws = {                     # Local name for the provider
+      source  = "hashicorp/aws" # Source address
+      version = "~> 5.0"        # Provider version to use
+    }
+  }
+  required_version = ">= 1.5.0" # Required Terraform version to use
+}
